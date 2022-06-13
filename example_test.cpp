d5eda078c317783677f16db12073d50520c59756
danxxo <90875721+danxxo@users.noreply.github.com>
2022-06-13 03:38:31 +0300

    Update example_test.cpp

--- example_test.cpp
+++ example_test.cpp
@@ -6,6 +6,7 @@
 ///LOAD NULL
 TEST(JSON, LOAD_NULL){
     using namespace BMSTU;
+
     std::string s = "null";
     std::stringstream ss;
     ss.str(s);
@@ -13,11 +14,11 @@ TEST(JSON, LOAD_NULL){
     Document last = Document({});
     EXPECT_EQ(first, last);
 }
-///LOAD NULL HAS NO EXCEPTIONS
 
 ///LOAD STRING
 TEST(JSON, LOADSTRING){
     using namespace BMSTU;
+
     std::stringstream ss;
     ss.str("\"some_string\"");
     Document first = Load(ss);
@@ -27,30 +28,75 @@ TEST(JSON, LOADSTRING){
     EXPECT_EQ(first, last);
 }
 
-///LOAD STRING EXCEPTIONS
-TEST(LOADSTRING_EXC, string_parsing_error){
+TEST(LOADSTRING, ESCAPE_SYMBOLS){
     using namespace BMSTU;
+
     std::stringstream ss;
-    ss.str("\"\0\"");
-    EXPECT_THROW(Document first = Load(ss),ParsingError);
+    ss.str("\"HELLO!\t\\there,\\\n"
+           "there will be the tabulation\ttabulation\nhere is\0 the end\"");
+    Document first = Load(ss);
+    Print(first,std::cout);
+    Document last = Document(std::string(("HELLO!\t\\there,\\\n"
+                                          "there will be the tabulation\ttabulation\nhere is\0 the end")));
+    EXPECT_EQ(first, last);
 }
 
-TEST(LOADSTRING_EXC, Unexpexted_end_of_line){
+///LOAD BOOL
+TEST(JSON, LOADBOOL_TRUE){
     using namespace BMSTU;
+
+    std::string s = "true";
     std::stringstream ss;
-    ss.str("\"p\nh\"some_string\"");
+    ss.str(s);
+    Document first = Load(ss);
+    Document last = Document(true);
+    EXPECT_EQ(first, last);
+}
+TEST(JSON, LOADBOOL_FALSE){
+    using namespace BMSTU;
+
+    std::string s = "false";
+    std::stringstream ss;
+    ss.str(s);
+    Document first = Load(ss);
+    Document last = Document(false);
+    EXPECT_EQ(first, last);
+}
+
+///LOAD BOOL EXCEPTION
+TEST(LOADBOOL, EXCEPTION){
+    using namespace BMSTU;
+
+    std::string s = "flse";
+    std::stringstream ss;
+    ss.str(s);
+    EXPECT_THROW(Document first = Load(ss), ParsingError);
+}
+
+///LOAD ARRAY
+TEST(JSON, LOADARRAY){
+    using namespace BMSTU;
+
+    std::stringstream ss;
+    ss.str("[true, true, null, \"str1\", \"str2\"   ]");
+    Document first = Load(ss);
+    Document last = Document(Array{true, true, {}, std::string("str1"), std::string("str2")});
+    EXPECT_EQ(first, last);
+}
+
+///LOAD ARRAY EXCEPTION
+TEST(LOADARRAY, EXCEPTION){
+    using namespace BMSTU;
+
+    std::stringstream ss;
+    ss.str("[[]");
     EXPECT_THROW(Document first = Load(ss), ParsingError);
 }
 
 
-TEST(sefew, wefwe){
-    std::string s;
-    s.push_back('a');
-    s.push_back('\n');
-    s.push_back('b');
-    std::cout << s;
 
-}
+
+///LOAD DICT
 
 
 
@@ -58,12 +104,13 @@ TEST(sefew, wefwe){
 
 
 
+///LOAD NUMBER
 
 
 
 
 
-
+///LOAD NODE EXCEPTION NOT EOF
 
 
 
